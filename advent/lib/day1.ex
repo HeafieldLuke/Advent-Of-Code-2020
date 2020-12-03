ade16c68af35334ec808ac10e01a82416355a890
Luke Heafield <44007671+HeafieldLuke@users.noreply.github.com>
2020-12-02 22:13:32 -0500

    Changed spacing

--- advent/lib/day1.ex
+++ advent/lib/day1.ex
@@ -8,51 +8,51 @@ defmodule Day1 do
 			end
     end
 
-		def start() do
-			input = read_file()
-			IO.puts(part1(input))
-			IO.puts(part2(input))
+	def start() do
+		input = read_file()
+		IO.puts(part1(input))
+		IO.puts(part2(input))
+	end
+
+	def two_sums_to_2020(num, []), do: {:empty, nil}
+
+	def two_sums_to_2020(num, [head | tail]) do
+		cond do
+			num + head == 2020 ->
+				{:ok, head}
+			tail == [] ->
+				{:empty, nil}
+			true ->
+				two_sums_to_2020(num, tail)
 		end
+	end
 
-		def two_sums_to_2020(num, []), do: {:empty, nil}
-
-		def two_sums_to_2020(num, [head | tail]) do
-			cond do
-				num + head == 2020 ->
-					{:ok, head}
-				tail == [] ->
-					{:empty, nil}
-				true ->
-					two_sums_to_2020(num, tail)
-			end
+	def part1([head | tail]) do
+		case two_sums_to_2020(head, tail) do
+			{:ok, result} -> head * result
+			{:empty, _} -> part1(tail)
 		end
+	end
 
-		def part1([head | tail]) do
-			case two_sums_to_2020(head, tail) do
-				{:ok, result} -> head * result
-				{:empty, _} -> part1(tail)
-			end
+	def three_sums_to_2020(num, []), do: {:empty, nil}
+
+	def three_sums_to_2020(num, [head | tail]) do
+		cond do
+			num + head < 2020 -> 
+				case two_sums_to_2020(num + head, tail) do
+					{:ok, result} -> {:ok, num * head * result }
+					{:empty, _} -> three_sums_to_2020(num,tail)
+				end
+			tail == [] ->
+				{:error, nil}
+			true -> three_sums_to_2020(num, tail)
 		end
+	end
 
-		def three_sums_to_2020(num, []), do: {:empty, nil}
-
-		def three_sums_to_2020(num, [head | tail]) do
-			cond do
-				num + head < 2020 -> 
-					case two_sums_to_2020(num + head, tail) do
-						{:ok, result} -> {:ok, num * head * result }
-						{:empty, _} -> three_sums_to_2020(num,tail)
-					end
-				tail == [] ->
-					{:error, nil}
-				true -> three_sums_to_2020(num, tail)
-			end
+	def part2([head | tail]) do
+		case three_sums_to_2020(head, tail) do
+			{:ok, result} -> result
+			{error, _} -> part2(tail)
 		end
-
-		def part2([head | tail]) do
-			case three_sums_to_2020(head, tail) do
-				{:ok, result} -> result
-				{error, _} -> part2(tail)
-			end
-		end
+	end
 end

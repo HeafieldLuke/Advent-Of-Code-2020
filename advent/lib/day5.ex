6acd6b41be114f606e41092d01dae06d33cf052d
Luke Heafield <heafieldluke3@gmail.com>
2020-12-05 23:54:12 -0500

    Day 5 part 1 complete

---
+++ advent/lib/day5.ex
@@ -0,0 +1,65 @@
+defmodule Day5 do
+
+    def read_file() do
+        case File.read("../input/day5.txt") do
+            {:ok, body} -> String.split(body, "\n")
+            {:error, msg} -> IO.puts(msg)
+        end
+    end
+
+    def start() do
+        input = read_file()
+        IO.puts(input |> part1)
+        IO.puts(input |> part2)
+    end
+
+    def part1(input) do
+        input
+        |> Enum.map(fn x -> locate_seat(x) end)
+        |> IO.inspect
+        |> find_highest_seat
+    end
+
+    def locate_seat(seat) do
+            a = String.slice(seat, 0..6) |> String.graphemes |> setup_search(127, {"F", "B"})
+            b = String.slice(seat, 7..String.length(seat)) |> String.graphemes |> setup_search(7, {"L", "R"})
+            IO.puts("#{a}, #{b}")
+            {a,b}
+    end
+
+    def find_highest_seat(seats) do
+        seats
+        |> Enum.map(fn x -> seat_id(x) end)
+        |> Enum.max
+    end
+
+    def setup_search(l, high, range) do
+        search(l, {0, high}, range)
+    end
+
+    def search([head | tail], {low, high}, range) do
+        {a, b} = range
+        cond do
+            tail === [] ->
+                cond do
+                    head === a -> low
+                    head === b -> high
+                end
+            true ->
+                cond do
+                    head === a ->
+                        search(tail, {low   , div(high,2) + div(low,2)}, range)
+                    head === b ->
+                        search(tail, {div(low,2) + div(high,2) + 1, high}, range)     
+                end
+        end
+        
+    end
+
+    def seat_id({ row, column }), do: row * 8 + column
+
+    def part2(input) do
+        IO.puts("Hi")
+    end
+
+end

cfc5f8f380825d49977868bc7b2afed9ae0f5283
Luke Heafield <heafieldluke3@gmail.com>
2020-12-09 00:03:35 -0500

    Day 8 part 2 complete

--- advent/lib/day8.ex
+++ advent/lib/day8.ex
@@ -14,7 +14,7 @@ defmodule Day8 do
 
     def start() do
         input = read_file()
-        input |> part1 |> IO.puts
+        input |> part1 |> (fn {_, state} -> IO.puts(state) end).()
         input |> part2 |> IO.puts
     end
 
@@ -24,21 +24,39 @@ defmodule Day8 do
 
     def process_rules(state, next, executed, instructions) do
         if Enum.member?(executed, next) do
-            state
+            {:crash, state}
         else 
-            {op, num} = instructions[next]
-            case op do
-                "nop" -> process_rules(state, next+1, [next | executed], instructions)
-                "jmp" -> process_rules(state, next + num, [next | executed], instructions)
-                "acc" -> process_rules(state + num,next + 1, [next | executed], instructions)
+            case instructions[next] do
+                nil -> {:exit, state}
+                {op, num} -> 
+                    case op do
+                        "nop" -> process_rules(state, next+1, [next | executed], instructions)
+                        "jmp" -> process_rules(state, next + num, [next | executed], instructions)
+                        "acc" -> process_rules(state + num,next + 1, [next | executed], instructions)
             end
+            end 
         end
         
     end
 
+    def fix(modified, address, original) do
+        case process_rules(0,0,[],modified) do
+            {:crash, _} ->
+                case original[address] do
+                    {"acc", _} ->
+                        fix(original, address + 1, original)
+                    instruction ->
+                        fix(Map.put(original, address, swap(instruction)), address + 1, original)
+                end
+            {:exit, state} -> state
+        end
+    end
+
+    def swap({"jmp", value}), do: {"nop", value}
+    def swap({"nop", value}), do: {"jmp", value}
 
     def part2(instructions) do
-        IO.puts("hi")
+        fix(instructions, 0, instructions)
     end
 
 end

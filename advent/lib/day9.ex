c84f999e6e5bbb08e9cf141a7797ef832637bca9
Luke Heafield <heafieldluke3@gmail.com>
2020-12-09 23:54:47 -0500

    Day 9 part 2 complete

--- advent/lib/day9.ex
+++ advent/lib/day9.ex
@@ -6,14 +6,14 @@ defmodule Day9 do
                 body
                 |> String.split("\n")
                 |> Enum.map(fn x -> String.to_integer(x) end)
-                |> Enum.split(25)
+ 
             {:error, msg} -> IO.puts(msg)
         end
     end
 
     def start() do
         input = read_file()
-        input |> part1 |> IO.puts
+        input |> Enum.split(25) |> part1 |> IO.puts
         input |> part2 |> IO.puts
     end
 
@@ -35,6 +35,26 @@ defmodule Day9 do
     def generate_sums(sums, []), do: sums
 
     def part2(input) do
-        
+        part1(input |> Enum.split(25))
+        |> find_contiguous_set(input)
     end
+
+    def find_contiguous_set(target, [head | tail]) do
+        case generate_contiguous_set([], target, [head | tail]) do
+            {:valid, list} -> Enum.min(list) + Enum.max(list)
+            {:invalid, _} -> find_contiguous_set(target, tail)
+        end
+    end
+
+    def generate_contiguous_set(acc, target, [head | tail]) do
+        case Enum.sum([head | acc]) do
+            n when n > target ->
+                {:invalid, n}
+            n when n < target ->
+                generate_contiguous_set([head | acc], target, tail)
+            n when n == target -> {:valid, [head | acc]}
+        end
+    end
+
+
 end
